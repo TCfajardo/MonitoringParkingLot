8fd0544f6d7e657dd3a9b8214f8e2cdf6707837d
HK-Kapyushon <nicolas.munevar01@uptc.edu.co>
2024-03-21 00:54:12 -0500

    rama

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 const { spawn } = require('child_process');
 
+const Docker = require('dockerode');
+const docker = new Docker({ host: 'http://localhost', port: 2375 });
+const { spawn } = require('child_process');
+const fs = require('fs');
+
 dotenv.config();
 
 const app = express();
@@ -65,43 +70,66 @@ const performHealthCheck = async () => {
 };
 
 const checkAndLaunchServers = async () => {
-    const inactiveServers = [];
+    // Función para obtener los contenedores y verificar si están activos
+    const checkContainers = () => {
+        return new Promise((resolve, reject) => {
+            docker.listContainers({ all: true }, (err, containers) => {
+                if (err) {
+                    console.error('Error al listar contenedores:', err);
+                    reject(err);
+                } else {
+                    // Filtrar los contenedores que contengan "server" en el nombre
+                    const serverContainers = containers.filter(container => container.Names.some(name => name.includes('server')));
+                    const activeServerContainers = serverContainers.filter(container => container.State === 'running');
+                    
+                    // Verificar si hay al menos 3 contenedores activos
+                    if (activeServerContainers.length >= 3) {
+                        resolve(activeServerContainers);
+                    } else {
+                        console.log('Hay menos de 3 Contenedores Servidores');
+                        console.log('Se lanzaran nuevos Contenedores de Servidores');
+                        const scriptPath = 'script2.bat';
+                        const batProcess = spawn('cmd', ['/c', scriptPath]);
 
-    for (const serverUrl of servers) {
-        const formattedTime = new Date().toISOString();
-        const options = { timeout: healthCheckInterval }; 
+                        batProcess.stdout.on('data', (data) => {
+                            console.log(`${data}`);
+                        });
 
-        try {
-            const response = await axios.get(serverUrl + '/ping', options);
-            const status = response.status === 200 ? 'Activo' : 'Inactivo';
-            inactiveServers.push({ server: serverUrl, status });
-            console.log(`[${formattedTime}] Se realizó el ping para ${serverUrl} - ${status}`);
-        } catch (error) {
-            console.error(`[${formattedTime}] Error en el ping para ${serverUrl}: ${error.message}`);
-            inactiveServers.push({ server: serverUrl, status: 'Inactivo' });
+                        batProcess.stderr.on('data', (data) => {
+                            console.error(`Error en el nuevo servidor: ${data}`);
+                        });
+                    }
+                }
+            });
+        });
+    };
+
+    try {
+        // Obtener los contenedores activos que contienen "server" en el nombre
+        const activeServerContainers = await checkContainers();
+        console.log('Contenedores de Servidores Activos: ', activeServerContainers.length);
+
+        // Realizar el lanzamiento de servidores inactivos (si es necesario)
+        if (activeServerContainers.length < 6) {
+            console.log('No se lanzarán nuevos servidores.');
+
+            const scriptPath = 'script.bat';
+            const batProcess = spawn('cmd', ['/c', scriptPath]); // Ejecutar el script bat
+
+            batProcess.stdout.on('data', (data) => {
+                console.log(`${data}`);
+            });
+
+            batProcess.stderr.on('data', (data) => {
+                console.error(`Error en el nuevo servidor: ${data}`);
+            });
+        } else {
+            console.log('No hay al menos 3 servidores activos, se lanzarán nuevos servidores.');
         }
-    }
-
-    // Realizar el lanzamiento de servidores inactivos
-    if (inactiveServers.length > 0) {
-        console.log(`Los siguientes servidores están inactivos o tienen tiempos de respuesta altos: ${inactiveServers}`);
-        
-        // Ejecutar el script para lanzar servidores
-        const { spawn } = require('child_process');
-        const newServerProcess = spawn('node', ['script.txt']);
-        
-        newServerProcess.stdout.on('data', (data) => {
-            console.log(`Nuevo servidor: ${data}`);
-        });
-        
-        newServerProcess.stderr.on('data', (data) => {
-            console.error(`Error en el nuevo servidor: ${data}`);
-        });
-    } else {
-        console.log('Todos los servidores están activos y respondiendo dentro del tiempo esperado.');
+    } catch (error) {
+        console.error('Error al verificar los contenedores:', error);
     }
 };
-
 
 // Llama a la función para verificar y lanzar servidores al inicio y luego en intervalos regulares
 checkAndLaunchServers();
@@ -129,7 +157,7 @@ server.on('error', (err) => {
     console.error('Error en el servidor HTTP:', err);
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Servidor de monitoreo escuchando en el puerto ${PORT}`);
 });
