fa8a26309152d8732daf46af1b266fd526840e5e
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-03-20 19:30:41 -0500

    data ws

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const wss = new WebSocket.Server({ server });
 const servers = process.env.SERVERS.split(',');
 const healthCheckInterval = process.env.HEALTH_CHECK_INTERVAL || 5000; 
 
-const services = process.env.SERVICES.split(','); 
+const responseTimes = [];
 
 const performHealthCheck = async () => {
     const healthStatus = [];
@@ -23,15 +23,36 @@ const performHealthCheck = async () => {
         const options = { timeout: 5000 }; 
 
         try {
+            const startTime = Date.now();
+
             const response = await axios.get(serverUrl + '/ping', options);
             const status = response.status === 200 ? 'Activo' : 'Inactivo';
-            healthStatus.push({ server: serverUrl, status });
-            console.log(`[${formattedTime}] Se realizó el ping para ${serverUrl} - ${status}`);
+            const responseTime = Date.now();
+            const timeDifference = responseTime - startTime;
+            console.log(`[${formattedTime}] Se realizó el ping para ${serverUrl} - ${status} - latencia : ${timeDifference}`);
+
+            const pingAndRequestsResponse = await axios.get(`${serverUrl}/ping-and-requests`);
+            const { totalRequests, errorRequests, postRequests, getRequests, patchRequests} = pingAndRequestsResponse.data;
+            console.log(`[${formattedTime}] totalRequests : ${totalRequests} - errorRequests : ${errorRequests} - POST/GET/PATCH ${postRequests}  ${getRequests} ${patchRequests} `);
+
+            responseTimes.push({ server: serverUrl, timeDifference });
+
+            healthStatus.push(
+                { server: 
+                    serverUrl, 
+                    status, 
+                    totalRequests, 
+                    errorRequests, 
+                    postRequests, 
+                    getRequests, 
+                    patchRequests,
+                    responseTimes
+                });
+
         } catch (error) {
             console.error(`[${formattedTime}] Error en el ping para ${serverUrl}: ${error.message}`);
             healthStatus.push({ server: serverUrl, status: 'Inactivo' });
         }
-        
     }
 
     // Emitir el estado de salud a todos los clientes WebSocket
