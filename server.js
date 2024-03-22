630afdeb0a3b9aaa9b5bb3fd863238b139480748
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-03-21 19:37:44 -0500

    Conexion serves y lanzamiento de scripts

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const dotenv = require('dotenv');
 const Docker = require('dockerode');
 const docker = new Docker({ host: 'http://localhost', port: 2375 });
 const { spawn } = require('child_process');
+const { exec } = require('child_process');
+
 const fs = require('fs');
 
 dotenv.config();
@@ -15,7 +17,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const servers = process.env.SERVERS.split(',');
+let SERVERS = [];
+
 const healthCheckInterval = process.env.HEALTH_CHECK_INTERVAL || 5000;
 
 const responseTimes = [];
@@ -23,7 +26,9 @@ const responseTimes = [];
 const performHealthCheck = async () => {
     const healthStatus = [];
 
-    for (const serverUrl of servers) {
+    for (const serverUrl of SERVERS) {
+        console.log("+++++++++++++++++ " + SERVERS)
+        console.log("+++++++++++++++++ " + serverUrl)
         const formattedTime = new Date().toISOString();
         const options = { timeout: 5000 };
 
@@ -44,8 +49,7 @@ const performHealthCheck = async () => {
 
             healthStatus.push(
                 {
-                    server:
-                        serverUrl,
+                    server: serverUrl,
                     status,
                     totalRequests,
                     errorRequests,
@@ -69,65 +73,54 @@ const performHealthCheck = async () => {
     });
 };
 
-// Emitir el estado de salud a todos los clientes WebSocket
-wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'healthCheck', data: healthStatus }));
-    }
-});
 
+// Función para ejecutar el script y generar el archivo .json con los servidores activos
+function runScript() {
+    exec('cmd /c Script.bat', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error al ejecutar el script: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Error de salida estándar del script: ${stderr}`);
+            return;
+        }
+        console.log(`Salida del script: ${stdout}`);
+        extractIPPorts(); // Después de ejecutar el script, extraemos las IPs y puertos
+    });
+}
 
-const checkAndLaunchServers = async () => {
-    // Función para obtener los contenedores y verificar si están activos
-    const checkContainers = () => {
-        return new Promise((resolve, reject) => {
-            docker.listContainers({ all: true }, (err, containers) => {
-                if (err) {
-                    console.error('Error al listar contenedores:', err);
-                    reject(err);
-                } else {
-                    // Filtrar los contenedores que contengan "server" en el nombre
-                    const serverContainers = containers.filter(container => container.Names.some(name => name.includes('server')));
-                    const activeServerContainers = serverContainers.filter(container => container.State === 'running');
-
-                    // Verificar si hay al menos 3 contenedores activos
-                    if (activeServerContainers.length < 3) {
-                        runScript2();
-                    } else {
-                        resolve(activeServerContainers);
-                    }
-                }
-            });
-        });
-    };
-
+// Función para extraer IPs y puertos del archivo container.json
+function extractIPPorts() {
     try {
-        // Obtener los contenedores activos que contienen "server" en el nombre
-        const activeServerContainers = await checkContainers();
-        console.log('Contenedores de Servidores Activos: ', activeServerContainers.length);
+        const containerData = JSON.parse(fs.readFileSync('containers.json', 'utf8'));
+        const ipPorts = containerData.map(container => `http://10.4.75.190:${container.PORTS.split('->')[0].split(':')[1]}`);
+        console.log("Lista de IP y Puertos:");
+        console.log(ipPorts); // Imprimir la lista de IPs y puertos en la consola
+        SERVERS = ipPorts;
+        console.log("SERVERS:", SERVERS);
+    } catch (error) {
+        console.error('Error al leer o parsear el archivo containers.json:', error);
+        // Puedes manejar el error aquí, por ejemplo, mostrando un mensaje al usuario o realizando una acción alternativa.
+        return ''; // Si hay un error, devuelve una cadena vacía
+    }
+}
 
-        // Realizar el lanzamiento de servidores inactivos (si es necesario)
-        if (activeServerContainers.length >= 3) {
-            console.log('No se lanzarán nuevos servidores.');
-
-            const scriptPath = 'script.bat';
-            const batProcess = spawn('cmd', ['/c', scriptPath]); // Ejecutar el script bat
-
-            batProcess.stdout.on('data', (data) => {
-                console.log(`${data}`);
-            });
-
-            batProcess.stderr.on('data', (data) => {
-                console.error(`Error en el nuevo servidor: ${data}`);
-            });
+// Función para verificar y lanzar servidores al inicio y luego en intervalos regulares
+const checkAndLaunchServers = async () => {
+    try {
+        if (SERVERS.length < 3) {
+            console.log('Se lanzarán nuevos servidores.');
+            runScript2(); // Llama al método para ejecutar el script2.bat y lanzar nuevos servidores
         } else {
-            console.log('Hay al menos 3 servidores activos, No se lanzarán nuevos servidores.');
+            console.log('Hay al menos 3 servidores activos, no se lanzarán nuevos servidores.');
         }
     } catch (error) {
-        console.error('Error al verificar los contenedores:', error);
+        console.error('Error al verificar y lanzar servidores:', error);
     }
 };
 
+// Función para ejecutar el script2.bat y lanzar nuevos servidores
 function runScript2() {
     console.log('Hay menos de 3 Contenedores Servidores');
     console.log('Se lanzarán nuevos Contenedores de Servidores');
@@ -153,8 +146,10 @@ function runScript2() {
     });
 }
 
+
 // Llama a la función para verificar y lanzar servidores al inicio y luego en intervalos regulares
 checkAndLaunchServers();
+setInterval(runScript, 10000);
 setInterval(checkAndLaunchServers, healthCheckInterval);
 
 
@@ -162,6 +157,7 @@ setInterval(checkAndLaunchServers, healthCheckInterval);
 wss.on('connection', (ws) => {
     console.log('Cliente WebSocket conectado');
 
+    runScript();
     // Realizar el primer chequeo de salud al conectar un cliente
     performHealthCheck();
 
@@ -183,3 +179,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Servidor de monitoreo escuchando en el puerto ${PORT}`);
 });
+
